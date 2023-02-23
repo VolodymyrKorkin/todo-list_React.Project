55175fbdc711cd4f37c55139f65c5a55ee895e07
Vladimir Korkin <korkin.dev@gmail.com>
2023-02-23 13:06:57 +0100

    add element tree in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+import React from "react";
+import ReactDOM from "react-dom";
+
+// create react element
+// const el = <h1>Hello World</h1>;
+
+// Using vanilla JS. How Babel transforms JSX into JS.
+// const el = React.createElement('h1', null, 'Hello World!!');
+
+// render element on a page
+// ReactDOM.render(el, document.getElementById('root'));
+
+// create element tree
+const el = (
+  <div>
+    <h1>My ToDo list</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(el, document.getElementById('root'));
