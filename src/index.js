5a26dd7f52e3db2edc5c9a7fb85236a7b7e9023e
Vladimir Korkin <korkin.dev@gmail.com>
2023-02-26 14:04:04 +0100

    add folder components, change structure of the project

--- src/index.js
+++ src/index.js
@@ -1,44 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// create react element
-// const el = <h1>Hello World</h1>;
-
-// Using vanilla JS. How Babel transforms JSX into JS.
-// const el = React.createElement('h1', null, 'Hello World!!');
-
-// render element on a page
-// ReactDOM.render(el, document.getElementById('root'));
-
-// create element tree
-// const el = (
-//   <div>
-//     <h1>My ToDo list</h1>
-//     <input placeholder="search" />
-//     <ul>
-//       <li>Learn React</li>
-//       <li>Build Awesome App</li>
-//     </ul>
-//   </div>
-// );
-
-// create react component
-const TodoList = () => {
-  return (
-    <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
-    </ul>
-  );
-};
-
-const AppHeader = () => {
-  return <h1>My ToDo list</h1>;
-};
-
-const SearchPanel = () => {
-  return <input placeholder="search" />
-};
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
   return (
