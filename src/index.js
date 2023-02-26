db057dcade85ddb7f4f51ec2a49b9be80b068ccf
Vladimir Korkin <korkin.dev@gmail.com>
2023-02-26 12:52:35 +0100

    add components TodoList, AppHeader, SearchPanel, App into index.js

--- src/index.js
+++ src/index.js
@@ -11,15 +11,44 @@ import ReactDOM from "react-dom";
 // ReactDOM.render(el, document.getElementById('root'));
 
 // create element tree
-const el = (
-  <div>
-    <h1>My ToDo list</h1>
-    <input placeholder="search" />
+// const el = (
+//   <div>
+//     <h1>My ToDo list</h1>
+//     <input placeholder="search" />
+//     <ul>
+//       <li>Learn React</li>
+//       <li>Build Awesome App</li>
+//     </ul>
+//   </div>
+// );
+
+// create react component
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Build Awesome App</li>
     </ul>
-  </div>
-);
+  );
+};
 
-ReactDOM.render(el, document.getElementById('root'));
+const AppHeader = () => {
+  return <h1>My ToDo list</h1>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+ 
