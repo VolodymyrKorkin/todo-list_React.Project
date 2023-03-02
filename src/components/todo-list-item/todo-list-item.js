7588fbd97937e238c0ee6c9df3662c84add28210
Vladimir Korkin <korkin.dev@gmail.com>
2023-03-02 10:38:16 +0100

    change functions ItemStatusFilter, TodoListItem to classes to add states

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,71 @@
-import React from 'react';
+import React from 'react'; 
+import { Component } from 'react'; /* {Component} === React.Component */
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
+// CLASS
+export default class TodoListItem extends Component { /* {Component} === React.Component */
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
+
+  render() {
+
+    const { label, important = false } = this.props;
+
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
+  
+    return (
+      <span className="todo-list-item">
+        <span
+          className="todo-list-item-label"
+          style={style}>
+          {label}
+        </span>
+  
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation" />
+        </button>
+  
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
   };
 
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
-      </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
 };
 
-export default TodoListItem;
+//  FUNCTION
+// const TodoListItem = ({ label, important = false }) => {
+
+//   const style = {
+//     color: important ? 'steelblue' : 'black',
+//     fontWeight: important ? 'bold' : 'normal'
+//   };
+
+//   return (
+//     <span className="todo-list-item">
+//       <span
+//         className="todo-list-item-label"
+//         style={style}>
+//         {label}
+//       </span>
+
+//       <button type="button"
+//               className="btn btn-outline-success btn-sm float-right">
+//         <i className="fa fa-exclamation" />
+//       </button>
+
+//       <button type="button"
+//               className="btn btn-outline-danger btn-sm float-right">
+//         <i className="fa fa-trash-o" />
+//       </button>
+//     </span>
+//   );
+// };
+
+// export default TodoListItem;
