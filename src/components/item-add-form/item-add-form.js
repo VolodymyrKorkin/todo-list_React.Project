05cca677cb3a9c5717d52b1c14b6a5a8e9be8c0f
Vladimir Korkin <korkin.dev@gmail.com>
2023-05-08 15:36:56 +0200

    add form for adding tasks. Add eventlistener onChange to have current value. Add eventlistener onSubmit to adding tasks to list

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,16 +3,32 @@ import React, { Component } from "react";
 import "./item-add-form.css";
 
 export default class ItemAddForm extends Component {
+  state = {
+    label: "",
+  };
+
+  onLabelChange = (event) => {
+    this.setState({
+      label: event.target.value, // 'event.target.value' contains current value of the input field
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault(); // cancel default behavior (reloading page after sending data to server)
+    this.props.onItemAdded(this.state.label);
+  };
+
   render() {
     return (
-      <div className="item-add-form">
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.onItemAdded("Hello World")}
-        >
-          Add Item
-        </button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done"
+        />
+        <button className="btn btn-outline-secondary">Add Item</button>
+      </form>
     );
   }
 }
