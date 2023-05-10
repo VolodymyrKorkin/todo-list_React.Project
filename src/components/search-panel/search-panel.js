7b07e91d1077a9cda03f78de8db2dbed889d16f2
Vladimir Korkin <korkin.dev@gmail.com>
2023-05-10 15:58:59 +0200

    add search implementation. add function OnSearchChange

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import './search-panel.css';
+import "./search-panel.css";
 
-const SearchPanel = () => {
-  return (
-    <input type="text"
-              className="form-control search-input"
-              placeholder="type to search" />
-  );
-};
+export default class SearchPanel extends Component {
+  state = {
+    term: "",
+  };
 
-export default SearchPanel;
+  onSearchChange = (event) => {
+    const term = event.target.value; // event.target.value - current value of input
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  };
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="type to search"
+        value={this.state.term} // make element controlled
+        onChange={this.onSearchChange} // send to the app every button press
+      />
+    );
+  }
+}
